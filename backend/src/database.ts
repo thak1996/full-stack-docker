6d3cb0d96eebffe3617cedba0819204d885ad3db
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2024-11-26 11:50:43 -0300

    Modificação base para lidar com o database do backend
    e ajuste do .env.example
    
    instalação do axios

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -4,7 +4,10 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "root",
-    database: process.env.DB_NAME || "app_db"
+    database: process.env.DB_NAME || "app_db",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 const createDriversTable = async () => {
